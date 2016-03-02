0cde0fd15003b184ed619ba86a7121869a7665d4
Santiago de Leon <sdeleon28@gmail.com>
2016-03-02 04:14:08 -0300

    Updated setup script for OpenSUSE

--- setup-opensuse.sh
+++ setup-opensuse.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-zypper install xclip
-python updateTranslate.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+zypper install xclip python-gtk-devel python-xlib
+${DIR}/updateTranslate.sh
